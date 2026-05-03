72d7de261e1ff9f449a5d82e42b04ebf8fddf638
marwan hussein <marawanlabana649@gmail.com>
2026-05-03 19:34:47 +0300

    modify to make the static board as png not svg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import tksvg
+from PIL import Image, ImageTk
 import os
 
 
@@ -8,51 +8,60 @@ def main():
     root = tk.Tk()
     root.title("Hnefatafl Game")
 
+    # used dimensions
     BOARD_SIZE = 1024
+    DISPLAY_WIDTH = 1820  # approximatly = 1024 * 16/9
+    DISPLAY_HEIGHT = 1024
     ACTOR_SIZE = 89
+    root.geometry(f"{DISPLAY_WIDTH}x{DISPLAY_HEIGHT}")
 
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    board_path = os.path.join(script_dir, "assets", "GameBoard.svg")
-
-    # customize icon
-    icon_path = os.path.join(script_dir, "assets", "icon.ico")
-    root.iconbitmap(icon_path)
+    # used paths
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    icon_path = os.path.join(current_dir, "assets", "icon.ico")
+    board_path = os.path.join(current_dir, "assets", "GameBoard.png")
 
     # verfy file exists to avoid crashes
     if not os.path.exists(board_path):
         print(f"Error: couldn't find the path: {board_path}")
         return
 
+    # customize icon
+    root.iconbitmap(icon_path)
+
     # create the canvas
     canvas = tk.Canvas(
         root,
-        width=BOARD_SIZE * (16 / 9),
-        height=BOARD_SIZE,
+        width=DISPLAY_WIDTH,
+        height=DISPLAY_HEIGHT,
         highlightthickness=0,
         bg="#F5EDDA",
     )  # to be with screen's width
-    canvas.pack()
+    canvas.pack(fill="both", expand=True)
 
-    # load the svg file (board)
+    # load the png file (GameBoard)
     try:
-        original_height = tksvg.SvgImage(file=board_path).height()
+        board_img = Image.open(board_path).resize(
+            (BOARD_SIZE, BOARD_SIZE), Image.LANCZOS
+        )
 
-        main_board = tksvg.SvgImage(file=board_path, scale=BOARD_SIZE / original_height)
+        # Convert to be as Tkinter Image
+        main_board = ImageTk.PhotoImage(board_img)
 
-        # Place the image on the canvas
-        # We anchor it to North-West (top-left corner) at coordinates 0,0
-        canvas.create_image(0, 0, image=main_board, anchor=tk.NW, tags="board")
-        canvas.tag_raise("board")
+        # place the image to be centered
+        canvas.create_image(
+            canvas_width // 2,
+            canvas_height // 2,
+            image=main_board,
+            anchor=tk.CENTER,
+            tags="board",
+        )
 
-        # IMPORTANT: Keep a reference to the image object.
-        # If this variable is garbage collected, the image will vanish from the UI.
+        # ref to the canvas
         canvas.board_image = main_board
 
     except Exception as e:
-        print(f"Failed to load SVG: {e}")
-    print(f"board height = ", original_height)
-    print("Board path:", board_path)
-    print("File exists:", os.path.exists(board_path))
+        print(f"Faild to load PNG: {e}")
+
     root.mainloop()
 
 
